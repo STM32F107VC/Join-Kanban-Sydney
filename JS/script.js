131317cb5cdf355c097df4904d3c26563765d00d
STM32F107VC <baer.sydney@outlook.com>
2023-11-01 20:49:41 +0100

    last commit before sleep
    Startet programming mobile contacts menu.

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
 async function init() {
     await includeHTML();
 }
+
+function openContactsOverlay() {
+    console.log('Contacts Overlay');
+    document.getElementById('contacts').classList.add('d-none');
+    document.getElementById('overlay-contacts').classList.remove('d-none');
+    document.getElementById('body-contacts').classList.add("flex", "x-center", "y-center");
+    document.getElementById('side-and-topbar-contacts').classList.add("opacity", "z-ind--1");
+    // document.getElementById('side-and-topbar-contacts').classList.add("z-ind-0");
+}
