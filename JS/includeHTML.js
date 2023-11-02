2d188d64a7779c8b7384afa030fd9a9bfb8f2c7e
STM32F107VC <baer.sydney@outlook.com>
2023-11-02 19:48:15 +0100

    finished contacts

--- JS/includeHTML.js
+++ JS/includeHTML.js
@@ -2,7 +2,7 @@ async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         let element = includeElements[i];
-        let file = element.getAttribute('w3-include-html'); // liesst in diesem fall den wert von der header.html datei aus
+        let file = element.getAttribute('w3-include-html');
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
--- JS/script.js
+++ JS/script.js
@@ -3,10 +3,15 @@ async function init() {
 }
 
 function openContactsOverlay() {
-    console.log('Contacts Overlay');
     document.getElementById('contacts').classList.add('d-none');
-    document.getElementById('overlay-contacts').classList.remove('d-none');
     document.getElementById('body-contacts').classList.add("flex", "x-center", "y-center");
     document.getElementById('side-and-topbar-contacts').classList.add("opacity", "z-ind--1");
-    // document.getElementById('side-and-topbar-contacts').classList.add("z-ind-0");
+    document.getElementById('overlay-contacts').classList.remove('d-none');
+}
+
+function closeContactOverlay() {
+    document.getElementById('contacts').classList.remove('d-none');
+    document.getElementById('body-contacts').classList.remove("flex", "x-center", "y-center");
+    document.getElementById('side-and-topbar-contacts').classList.remove("opacity", "z-ind--1");
+    document.getElementById('overlay-contacts').classList.add('d-none');
 }
