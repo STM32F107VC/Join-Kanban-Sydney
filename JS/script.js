ac4f0bc913aec57082c9201cae830bfc5724a2a9
STM32F107VC <baer.sydney@outlook.com>
2023-11-04 21:14:22 +0100

    Added JS doc

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,52 @@
+async function init() {
+    await includeHTML();
+}
+
 /* Declare variable */
 let signUp = [];
 let users = [];
 
 
-async function init() {
-    await includeHTML();
-}
-
-
-function signUpSection() {
+function openSignUpSection() {
     document.getElementById('loggin').classList.add('d-none');
     document.getElementById('sign-up').classList.remove('d-none');
 }
 
-function generateAccount() {
+function closeSignUpSection() {
+    document.getElementById('loggin').classList.remove('d-none');
+    document.getElementById('sign-up').classList.add('d-none');
+}
 
+/**
+ * This function is used to take information of the registration process
+ * @param {string} name This is the name of the person registering
+ * @param {string} email This is the email of the person registering 
+ * @param {string} password This is the password of the person registering
+ * @param {string} comparePassword This is the password to compare of the person registering
+ */
+function addNewUser() {
+    console.log('addNewUser function');
+    let name = document.getElementById("new-name");
+    let email = document.getElementById("new-email");
+    let password = document.getElementById("new-password");
+    let comparePassword = document.getElementById("compare-password");
+    saveNewUser(name, email, password, comparePassword);
+}
+
+function saveNewUser(name, email, password, comparePassword) {
+    if (password.value == comparePassword.value) {
+        console.log('Registration succesfull.');
+        let addUser = {
+            "Name": name,
+            "Email": email,
+            "Password": password,
+        }
+        users.push(addUser);
+        name.value = "";
+        email.value = "";
+        password.value = "";
+        comparePassword.value = "";
+    } else { console.log('Registration failed, check input!'); }
 }
 
 
